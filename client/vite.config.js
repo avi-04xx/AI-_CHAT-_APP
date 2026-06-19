01dd7630a02e9f92c0f4e8aba12c5012892ae29f
avi.04xx <avisvm12@gmail.com>
2026-06-19 15:22:32 +0530

    fix vite config for github pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AI-_CHAT-_APP/",     // ← This is very important
-  server: {
-    port: 5173,
-    strictPort: true
-  }
+  base: "/AI-_CHAT-_APP/",   // ← Important for GitHub Pages
 });
